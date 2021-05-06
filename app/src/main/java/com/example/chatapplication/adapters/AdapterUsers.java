f0010535c45984e14fdf2cd557f0dc578287b84f
NSarvari <63661686+NSarvari@users.noreply.github.com>
2021-05-07 02:40:22 +0300

    Show registered users

--- app/src/main/java/com/example/chatapplication/adapters/AdapterUsers.java
+++ app/src/main/java/com/example/chatapplication/adapters/AdapterUsers.java
@@ -2,13 +2,17 @@ package com.example.chatapplication.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.chatapplication.ChatActivity;
+import com.example.chatapplication.R;
 import com.example.chatapplication.moduls.ModulUser;
 
 import java.util.List;
@@ -19,41 +23,53 @@ public class AdapterUsers extends RecyclerView.Adapter<AdapterUsers.MyHolder> {
     List<ModulUser> userList;
 
     //constructor
-    public AdapterUsers(List<ModulUser> userList){
+    public AdapterUsers(Context context,List<ModulUser> userList){
+
+        this.context = context;
         this.userList=userList;
     }
     @NonNull
     @Override
-    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        //inflate layout(row_user.xml)
+        View view = LayoutInflater.from(context).inflate(R.layout.row_users,viewGroup,false);
+        return new MyHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyHolder myHolder, int i) {
         //get data
-        String hisUID=userList.get(i).getUid();
-        
+        String userName=userList.get(i).getName();
+        String userEmail = userList.get(i).getEmail();
+
+        //set data
+        myHolder.mNameTv.setText(userName);
+        myHolder.mEmailTv.setText(userEmail);
+
         //handle item click
-        myHolder.itemView.setOnClickListener((v)->{
-            /*click user from user list to start messaging
-             *start activity by putting UID of receiver
-             * we will use the UID to identify the user we are gonna chat with
-             */
-            Intent intent= new Intent(context, ChatActivity.class);
-            intent.putExtra("hisUid", hisUID);
-            context.startActivity(intent);
+        myHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, ""+userEmail,Toast.LENGTH_SHORT).show();
+            }
         });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return userList.size();
     }
 
     //view holder class
-    class MyHolder extends RecyclerView.ViewHolder{
+     class MyHolder extends RecyclerView.ViewHolder{
+        TextView mNameTv, mEmailTv;
         public MyHolder (@NonNull View itemView){
+
             super(itemView);
+
+            //init views
+            mNameTv = itemView.findViewById(R.id.nameTv);
+            mEmailTv = itemView.findViewById(R.id.emailEt);
         }
     }
 }
