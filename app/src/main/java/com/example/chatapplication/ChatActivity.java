6447f87f0c575ff45b92565d49f4919ec7d35e32
NSarvari <63661686+NSarvari@users.noreply.github.com>
2021-05-10 01:35:59 +0300

    send message

--- app/src/main/java/com/example/chatapplication/ChatActivity.java
+++ app/src/main/java/com/example/chatapplication/ChatActivity.java
@@ -1,14 +1,32 @@
 package com.example.chatapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class ChatActivity extends AppCompatActivity {
 
@@ -19,6 +37,14 @@ public class ChatActivity extends AppCompatActivity {
     TextView nameTv;
     EditText messageEt;
     ImageButton sendBtn;
+
+    FirebaseAuth firebaseAuth;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference userDbRef;
+
+    String hisUid;
+    String myUid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +60,113 @@ public class ChatActivity extends AppCompatActivity {
         nameTv = findViewById(R.id.nameTv);
         messageEt = findViewById(R.id.messageEt);
         sendBtn = findViewById(R.id.sendBtn);
+
+        /* On clicking user from users list we have passed user's UID using intent
+         *so get that UID here to get the name and start chat with that user
+         */
+        Intent intent = getIntent();
+        hisUid = intent.getStringExtra("hisUid");
+
+        //firebase auth instance
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        userDbRef = firebaseDatabase.getReference("Users");
+        //search user to get that user's info
+        Query userQuery = userDbRef.orderByChild("uid").equalTo(hisUid);
+        //get user name
+        userQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                //check until required info in received
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    //get data
+                    String name = ""+ds.child("name").getValue();
+                    //set data
+                    nameTv.setText(name);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //click button to send message
+        sendBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //get text from edit text
+                String message = messageEt.getText().toString().trim();
+                //check if text is empty or not
+                if (TextUtils.isEmpty(message)){
+                    //text empty
+                    Toast.makeText(ChatActivity.this,"Cannot send the empty message...",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    //text not empty
+                    sendMessage(message);
+                }
+            }
+        });
+    }
+
+    private void sendMessage(String message) {
+        /* "Chats" node will be created that will contain all chats
+         * whenever user sends message it will create new child in "Chats" node and that child will contain
+         * the following key values
+         * sender: UID of sender
+         * receiver: UID id receiver
+         * message: the actual message
+         */
+        DatabaseReference databaseReference= FirebaseDatabase.getInstance().getReference();
+
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender", myUid);
+        hashMap.put("receiver", hisUid);
+        hashMap.put("message",message);
+        databaseReference.child("Chats").push().setValue(hashMap);
+
+        //reset editText after sending message
+        messageEt.setText("");
+    }
+
+    private void checkUserStatus() {
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user!= null){
+            //user signed in stay here
+            //set email of logged in user
+            //mProfileTv.setText(user.getEmail());
+        }
+        else {
+            //user not signed in , go to main activity
+            startActivity(new Intent(this,MainActivity.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+        if (id==R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
